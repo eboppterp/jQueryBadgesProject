5c211286ac3f078b55b454e42450d4a955409650
eboppterp <32872998+eboppterp@users.noreply.github.com>
2017-10-17 11:25:43 -0400

    added .json to url

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,7 +1,7 @@
 $(function() {
   //call to my codeschool report card
   jQuery.ajax({
-    url: 'https://www.codeschool.com/users/2727904',
+    url: 'https://www.codeschool.com/users/2727904.json',
     dataType: 'jsonp',
     success: function(response) {
       // handle response
